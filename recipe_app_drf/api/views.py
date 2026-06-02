cf2596ec72545b7c81127c6aceced32b0fb99e8e
katsukikuroda <katsukikuroda@gmail.com>
2026-06-03 00:46:29 +0900

    views modified

--- recipe_app_drf/api/views.py
+++ recipe_app_drf/api/views.py
@@ -1,7 +1,13 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from .models import Category
 from .serializers import CategorySerializer
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
